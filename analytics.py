118fa320b90e73a263448a74b792576ebe08b21a
wiTTyMindsTech <63321726+wittymindstech@users.noreply.github.com>
2022-01-08 13:34:23 +0530

    Update analytics.py

--- analytics.py
+++ analytics.py
@@ -1,3 +1,29 @@
-import ssh
+import os
+import boto3
+import pandas as pd
 
-print ("This is my file")
+
+AWS_S3_BUCKET = "datanalytics"
+AWS_ACCESS_KEY_ID = ""
+AWS_SECRET_ACCESS_KEY = ""
+AWS_SESSION_TOKEN = "123"
+
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+)
+
+pd.set_option("display.max_rows", None)
+
+response = s3_client.get_object(Bucket=AWS_S3_BUCKET, Key="text/blood_data.csv")
+
+status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+
+if status == 200:
+    print(f"Successful S3 get_object response. Status - {status}")
+    books_df = pd.read_csv(response.get("Body"))
+    print(books_df)
+else:
+    print(f"Unsuccessful S3 get_object response. Status - {status}")
+
